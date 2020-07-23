fe161804d8fa0993f516878661101b71e037833c
jhardy6 <jhardy6@msudenver.edu>
2020-07-23 13:47:44 -0600

    Update Deploy-Application.ps1

--- Deploy-Application.ps1
+++ Deploy-Application.ps1
@@ -169,7 +169,7 @@ Try {
 		}
 
 		## <Perform Installation tasks here>
-		$exitCode = Execute-Process -Path "$dirFiles\setup.exe" -Parameters "-inputFile `$dirSupportFiles\installer_input.txt` -activationPropertiesFile `$dirSupportFiles\activate.ini`" -WaitForMsiExec -WindowStyle "Hidden" -PassThru
+		$exitCode = Execute-Process -Path "$dirFiles\setup.exe" -Parameters "-inputFile `"$dirSupportFiles\installer_input.txt`" -activationPropertiesFile `"$dirSupportFiles\activate.ini`"" -WindowStyle "Hidden" -PassThru
 		If (($exitCode.ExitCode -ne "0") -and ($mainExitCode -ne "3010")) { $mainExitCode = $exitCode.ExitCode }
 
 		##*===============================================
