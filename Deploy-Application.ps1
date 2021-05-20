ccb44f6949ccfad388b1827789090398a4491d7a
jhardy6 <jhardy6@msudenver.edu>
2021-05-20 10:42:48 -0600

    Changed setup name to match downloaded installer

--- Deploy-Application.ps1
+++ Deploy-Application.ps1
@@ -161,7 +161,7 @@ Try {
 		}
 
 		## <Perform Installation tasks here>
-		$exitCode = Execute-Process -Path "$dirFiles\setup.exe" -Parameters "-inputFile `"$dirSupportFiles\installer_input.txt`" -activationPropertiesFile `"$dirFiles\activate.ini`"" -WindowStyle "Hidden" -PassThru
+		$exitCode = Execute-Process -Path "$dirFiles\matlab_R2021a_win64.exe" -Parameters "-inputFile `"$dirSupportFiles\installer_input.txt`" -activationPropertiesFile `"$dirFiles\activate.ini`"" -WindowStyle "Hidden" -PassThru
 		If (($exitCode.ExitCode -ne "0") -and ($mainExitCode -ne "3010")) { $mainExitCode = $exitCode.ExitCode }
 
 		##*===============================================
